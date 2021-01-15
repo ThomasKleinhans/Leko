ae893a88758b670e5176da7c7912d30c68b8a816
Thomas KLEINHANS <kleinhans.thomas@gmail.com>
2021-01-15 13:52:32 +0100

    first implementation of contentfull content in project page

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,18 +2,32 @@
 	<div class="Projects" ref="wrapper">
 		<main role="main">
             <div>
-                
+                {{title}}
+				<div v-for="(project) in projectsDetails" :key="project">{{project.fields.title}}</div>
             </div>
 		</main>
 	</div>
 </template>
 
-<script>
+<script>	
+import { mapState, mapGetters } from 'vuex'
+
 export default {
 
 	mounted () {
 		setTimeout(() => {
 			this.$refs.wrapper.classList.add('active')
+		})
+	},
+
+	mounted () {
+		this.$store.dispatch('projects/fetchProjectsInfos')
+	},
+
+	computed: {
+		...mapGetters('projects', {
+			title: 'getTitle',
+			projectsDetails: 'getProjectsDetails'
 		})
 	}
 
---
+++ store/projects.js
@@ -0,0 +1,39 @@
+import contentful from '~/plugins/contentful.js'
+
+export const state = () => ({
+    datas: {},
+    projectsDetails: {},
+	isLoaded: false
+})
+
+export const getters = {
+    getTitle: state => state.datas.title,
+    getProjectsDetails: state => state.projectsDetails
+}
+
+export const mutations = {
+	FETCH_PROJECTS_FILTERS (state, datas) {
+		state.datas = datas
+		state.isLoaded = true
+	},
+	FETCH_PROJECTS_DETAILS (state, datas) {
+		state.projectsDetails = datas
+	},
+}
+
+
+export const actions = {
+	async fetchProjectsInfos ({ commit }) {
+		let dataFilters = await contentful.getEntries({ 'content_type': 'projectsFilters' })
+        
+        commit('FETCH_PROJECTS_FILTERS', dataFilters.items[0].fields)
+
+        let dataDetails = await contentful.getEntries({ 
+            'content_type': 'projectsDetails',
+            order: 'sys.createdAt'
+        })
+        
+        commit('FETCH_PROJECTS_DETAILS', dataDetails.items)
+        
+	}
+}
