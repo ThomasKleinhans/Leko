287dee9e7fbd05ed873d490de9a3b63429995e16
Thomas KLEINHANS <kleinhans.thomas@gmail.com>
2021-01-15 11:01:37 +0100

    add scss basics to the project page

---
+++ pages/projects.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="Projects" ref="wrapper">
+		<main role="main">
+            <div>
+                
+            </div>
+		</main>
+	</div>
+</template>
+
+<script>
+export default {
+
+	mounted () {
+		setTimeout(() => {
+			this.$refs.wrapper.classList.add('active')
+		})
+	}
+
+}
+</script>
